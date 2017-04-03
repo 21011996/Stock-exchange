534d08ae75830a5118445caf91f07517f49e26c8
Ilya Kachalskiy <21011996@mail.ru>
2017-04-03 22:52:21 +0300

    Byte[]

--- src/main/java/messages/Record.java
+++ src/main/java/messages/Record.java
@@ -17,11 +17,29 @@ public class Record {
         this.message = message;
     }
 
-    public static Record fromByteBuffer(ByteBuffer buffer) {
-        String name = stringFromBB(buffer);
+    public static Record fromByteBuffer(byte[] buffer) {
+        /*String name = stringFromBB(buffer);
         String header = stringFromBB(buffer);
-        String message = stringFromBB(buffer);
-        return new Record(name, header, message);
+        String message = stringFromBB(buffer);*/
+        int nameLenght = buffer[0];
+        StringBuilder name = new StringBuilder();
+        int i = 1;
+        for (int j = i; j < nameLenght + 1; j++) {
+            name.append((char) buffer[j]);
+        }
+        i += nameLenght;
+        int headerLenght = buffer[i];
+        StringBuilder header = new StringBuilder();
+        for (int j = i + 1; j < headerLenght + 1 + i; j++) {
+            header.append((char) buffer[j]);
+        }
+        i += headerLenght + 1;
+        int messageLenght = buffer[i];
+        StringBuilder message = new StringBuilder();
+        for (int j = i + 1; j < messageLenght + 1 + i; j++) {
+            message.append((char) buffer[j]);
+        }
+        return new Record(name.toString(), header.toString(), message.toString());
     }
 
     private static String stringFromBB(ByteBuffer buffer) {
@@ -37,19 +55,25 @@ public class Record {
         }
     }
 
-    public ByteBuffer toByteBuffer() {
-        ByteBuffer buffer = ByteBuffer.allocate(2048);
+    public byte[] toByteBuffer() {
+        byte[] answer = new byte[name.length() + 1 + header.length() + 1 + message.length() + 1];
+        int i = 0;
+        answer[i] = (byte) name.length();
+        for (int j = 0; j < name.length(); j++) {
+            answer[j + i + 1] = (byte) name.charAt(j);
+        }
+        i += name.length() + 1;
+        answer[i] = (byte) header.length();
+        for (int j = 0; j < header.length(); j++) {
+            answer[j + i + 1] = (byte) header.charAt(j);
+        }
+        i += header.length() + 1;
+        answer[i] = (byte) message.length();
+        for (int j = 0; j < message.length(); j++) {
+            answer[j + i + 1] = (byte) message.charAt(j);
+        }
 
-        ByteBuffer nameBuffer = stringToBB(name);
-        buffer.put(nameBuffer);
-
-        ByteBuffer headerBuffer = stringToBB(header);
-        buffer.put(headerBuffer);
-
-        ByteBuffer messageBuffer = stringToBB(message);
-        buffer.put(messageBuffer);
-
-        return (ByteBuffer) buffer.flip();
+        return answer;
     }
 
     private ByteBuffer stringToBB(String s) {
