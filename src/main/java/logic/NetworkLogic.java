c0d8b2bb3e5dc428765424e09954743855207609
Kirill <timofeev.kirill.a@gmail.com>
2017-04-03 13:52:47 +0300

    NetworkLogic interface

--- src/main/java/logic/NetworkLogic.java
+++ src/main/java/logic/NetworkLogic.java
@@ -8,24 +8,14 @@ import java.util.function.Consumer;
  * Created by heat_wave on 30.03.17.
  */
 
-public class NetworkLogic {
-    //private Map<String, NetworkInfo> addressBook = new Map<>();
-
+interface NetworkLogic {
     /**
      * A handle that sends a message to a node over the network.
      * @param node The addressee
      * @param message The message
      */
-    public void send(String node, Message message) {
-        //TODO: Parse the network information from the node
-    }
+    void send(String node, Message message);
+    //TODO: Parse the network information from the node
 
-    /**
-     * The "real" message transfer mechanism. Uses a node's network address to send the data.
-     */
-    private void send() {};
-
-    public void addMessageHandler(Consumer<? super Message> handler) {
-        //todo ensure that the consumer is called when messages arrive
-    }
+    void addMessageHandler(Consumer<? super Message> handler);
 }
